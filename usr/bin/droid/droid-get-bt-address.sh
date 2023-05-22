8ace4d1b7d89556aa9d43c1c0bbd87e47f6f4b86
Bardia Moshiri <fakeshell@bardia.tech>
2023-05-22 13:33:11 -0400

    disable sandboxing in bluebinder

--- usr/bin/droid/droid-get-bt-address.sh
+++ usr/bin/droid/droid-get-bt-address.sh
@@ -4,3 +4,5 @@
 find /var/lib/bluetooth -maxdepth 1 -iname '*:*:*:*:*:*' | cut -d/ -f 5 > /var/lib/bluetooth/board-address
 chown root:root /var/lib/bluetooth/board-address
 chmod 644 /var/lib/bluetooth/board-address
+
+rfkill unblock bluetooth
